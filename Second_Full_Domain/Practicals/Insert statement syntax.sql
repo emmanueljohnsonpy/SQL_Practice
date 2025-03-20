77f598294dd232a0feb1e7d74664a9d98ee89f62
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2025-03-20 23:57:32 +0530

    insert

---
+++ Second_Full_Domain/Practicals/Insert statement syntax.sql
@@ -0,0 +1,6 @@
+INSERT INTO student (firstname, lastname, email)
+VALUES ('Cristiano', 'Ronaldo', 'ronaldo@gmail.com');
+
+INSERT INTO student (firstname, lastname, email)
+VALUES ('Cristiano', 'Ronaldo', 'ronaldo@gmail.com'),
+       ('Lionel', 'Messi', 'messi@gmail.com');
